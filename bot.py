2397b1375ce54da1447f2d87740a942c298eaf9d
Oralov Ogabek <oralovogabek@gmail.com>
2026-09-16 20:11:55 +0500

    update project

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-```python
 import asyncio
 import os
 
@@ -12,17 +11,12 @@ from handlers.start import router as start_router
 from handlers.tasks import router as tasks_router
 
 
-# =========================
-# HTTP SERVER
-# =========================
-
 async def home(request):
     return web.Response(text="Bot is running!")
 
 
 async def start_http_server():
     app = web.Application()
-
     app.router.add_get("/", home)
 
     port = int(os.environ.get("PORT", 10000))
@@ -30,26 +24,16 @@ async def start_http_server():
     runner = web.AppRunner(app)
     await runner.setup()
 
-    site = web.TCPSite(
-        runner,
-        "0.0.0.0",
-        port
-    )
-
+    site = web.TCPSite(runner, "0.0.0.0", port)
     await site.start()
 
     print(f"HTTP server ishga tushdi: {port}")
 
 
-# =========================
-# BOT
-# =========================
-
 async def start_bot():
     create_database()
 
     bot = Bot(token=BOT_TOKEN)
-
     dp = Dispatcher()
 
     dp.include_router(start_router)
@@ -63,16 +47,10 @@ async def start_bot():
         await bot.session.close()
 
 
-# =========================
-# MAIN
-# =========================
-
 async def main():
     await start_http_server()
-
     await start_bot()
 
 
 if __name__ == "__main__":
     asyncio.run(main())
-```
